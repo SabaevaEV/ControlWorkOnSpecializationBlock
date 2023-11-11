c32a6820a3d3a0622a03ef67eac5e9994cb56cb0
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 00:22:06 +0400

    Added changes folder 'View'

---
+++ View.py
@@ -0,0 +1,11 @@
+def View ():
+    menu = int(input('Заметки -Общее меню: \n'
+                    '1 - Создать заметку\n'
+                    '2 - Edit note title\n'
+                    '3 - Edit note body\n'
+                    '4 - View for select note\n'
+                    '5 - View for date\n'
+                    '6 - View all notes\n'
+                    '7 - Удалить заметку\n'
+                    '8 - Выход\n'))
+    return menu
