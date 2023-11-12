873c356ecd5fdf72860abafe11ecde7a7b536e16
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 12:47:48 +0400

    Added changes

--- operations.py
+++ operations.py
@@ -14,21 +14,38 @@ def add_note(self, title, body):
     self.save_notes()
     print('Заметка успешно сохранена')
 
-# def read_notes():
-#     filter_date = input("Введите дату для фильтрации (в формате ГГГГ-ММ-ДД): ")
-#     if not notes:
-#         print("Заметок нет")
-#         return
-#     if not filter_date:
-#         for note in notes:
-#             print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
-#     else:
-#         filtered_notes = [note for note in notes if note["date"].startswith(filter_date)]
-#         if filtered_notes:
-#             for note in filtered_notes:
-#                 print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
-#         else:
-#             print("Заметок с указанной датой нет")
+def read_notes():
+    filter_date = input("Введите дату для фильтрации (в формате ГГГГ-ММ-ДД): ")
+    if not notes:
+        print("Заметок нет")
+        return
+    if not filter_date:
+        for note in notes:
+            print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
+    else:
+        filtered_notes = [note for note in notes if note["date"].startswith(filter_date)]
+        if filtered_notes:
+            for note in filtered_notes:
+                print(f"{note['id']}. {note['title']} ({note['date']})\n{note['msg']}\n")
+        else:
+            print("Заметок с указанной датой нет")
+
+def read_notes_by_date(self):
+    date = input("Введите дату (ГГГГ-ММ-ДД): ")
+    # try:
+    #     date_check = datetime.strptime(date, '%d-%m-%Y').date()
+    # except ValueError:
+    #     raise ValueError('Invalid date format. Please enter the date in dd-mm-yy format.')
+
+    filter_notes = []
+    for note in self.notes:
+        if note.date.split()[0] == date:
+            filter_notes.append(note)
+    if not filter_notes:
+        print("Заметок нет")
+    return filter_notes
+
+
 def view_all_notes(self):
     if self.notes:
         print("Все заметки")
