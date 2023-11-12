553ab92cfad25e38c11a28f1f06590e27e21bbfd
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 13:32:21 +0400

    Added changes

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ if __name__ == "__main__":
         elif number == "3":
             notebook.edit_note_body()
         elif number == "4":
-            delete_note()
+            notebook.view_filter_note()
+            # if selected_note is not None:
+            #     notebook.view_filter_note(filter_note.note_id, filter_note)
+            # else:
+            #     print("Заметка не найдена")
         elif number == "5":
             notes = notebook.read_notes_by_date()
             if notes:
@@ -33,43 +37,3 @@ if __name__ == "__main__":
             break
         else:
             print("Неверная команда")
-
-
-def main():
-    note_app = noteApp.NoteApp()
-    while True:
-        num = view.user_input()
-        if num == 1:
-            title, body = view.input_1()
-            note_app.add_note(title=title, body=body)
-        elif num == 2:
-            note_id, new_title = view.input_2()
-            note_app.edit_note_title(note_id=note_id, new_title=new_title)
-        elif num == 3:
-            note_id, new_body = view.input_3()
-            note_app.edit_note_body(note_id=note_id, new_body=new_body)
-        elif num == 4:
-            selected_note = view.input_4(note_app.notes)
-            if selected_note is not None:
-                note_app.view_selected_note(selected_note.note_id, selected_note)
-            else:
-                print("Note not found.")
-        elif num == 5:
-            notes = note_app.select_notes_by_date()
-            if notes:
-                for note in notes:
-                    print(note)
-        elif num == 6:
-            note_app.view_all_notes()
-        elif num == 7:
-            note_id = view.input_7()
-            note_app.delete_note(note_id=note_id)
-        elif num == 8:
-            print('Goodbye')
-            break
-        else:
-            print('Error: Invalid number, choice number is menu.')
-            input("Press the key to continue: ")
-
-if __name__ == '__main__':
-    main()
