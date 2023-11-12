803823ffc2c934787dd47494952e21fcea5704d6
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 13:22:52 +0400

    Added changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import noteBook as noteBook
+
 import view
 from operations import *
 from view import View
@@ -6,17 +8,28 @@ if __name__ == "__main__":
     # notes = load_notes()
     # notebook = noteApp.NoteApp()
     View()
+    notebook = noteBook.NoteOperation()
     while True:
         number = view.menu
         if number == "1":
-            add_note()
+            notebook.add_note()
         elif number == "2":
-            read_notes()
+            notebook.edit_note_tit()
         elif number == "3":
-            edit_note()
+            notebook.edit_note_body()
         elif number == "4":
             delete_note()
         elif number == "5":
+            notes = notebook.read_notes_by_date()
+            if notes:
+                for note in notes:
+                    print(note)
+        elif number == "6":
+            notebook.view_all_notes()
+        elif number == "7":
+            notebook.delete_note()
+        elif number == "8":
+            print("Выход")
             break
         else:
             print("Неверная команда")
--- operations.py
+++ operations.py
@@ -2,91 +2,91 @@ import json
 from datetime import datetime
 from note import Note
 
-
-def __init__(self):
-    self.notes = []
-    self.load_notes()
-
-def add_note(self, title, body):
-    note_id = len(self.notes) + 1
-    note = Note(note_id, title, body)
-    self.notes.append(note)
-    self.save_notes()
-    print('Заметка успешно сохранена')
-
-
-def read_notes_by_date(self):
-    date = input("Введите дату (ГГГГ-ММ-ДД): ")
-    # try:
-    #     date_check = datetime.strptime(date, '%d-%m-%Y').date()
-    # except ValueError:
-    #     raise ValueError('Invalid date format. Please enter the date in dd-mm-yy format.')
-
-    filter_notes = []
-    for note in self.notes:
-        if note.date.split()[0] == date:
-            filter_notes.append(note)
-    if not filter_notes:
-        print("Заметок нет")
-    return filter_notes
-
-def view_filter_note(self, note_id, note):
-    print(f"ID: {note.note_id}")
-    print(f"Title: {note.title}")
-    print(f"Body: {note.body}")
-    print(f"Date: {note.date}")
-
-def view_all_notes(self):
-    if self.notes:
-        print("Все заметки")
-        for note in self.notes:
-            print(note)
-        input("Нажмите номер для продолжения")
-    else:
-        print("Заметки не найдены")
-def edit_note_tit(self, note_id, new_title):
-    for note in self.notes:
-        if note.note_id == note_id:
-            note.title = new_title
-            note.date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            self.save_notes()
-            print("Заголовок заметки успешно отредактирована")
-            return
-    print(f"Заметка с id {note_id} не найдена")
-
-def edit_note_body(self, note_id, new_body):
-    for note in self.notes:
-        if note.note_id == note_id:
-            note.body = new_body
-            note.date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            self.save_notes()
-            print("Заметка успешно отредактирована")
-            return
-    print(f"Заметка с id {note_id} не найдена")
-
-def delete_note(self, note_id):
-    for note in self.notes:
-        if note.note_id == note_id:
-            self.notes.remove(note)
-            self.save_notes()
-            print("Заметка удалена")
-            return
-    print(f"Заметка с id {note_id} не найдена")
-
-def save_notes(self):
-    data = []
-    for note in self.notes:
-        note_data = {'note_id': note.note_id, 'title': note.title, 'body': note.body, 'date': note.date}
-        data.append(note_data)
-    with open("notes.json", "w") as file:
-        json.dump(data, file, indent=4)
-
-def load_notes(self):
-    try:
-        with open("notes.json", "r") as file:
-            data = json.load(file)
-            for note_data in data:
-                note = Note(note_data['note_id'], note_data['title'], note_data['body'], note_data['data'])
-                self.notes.append(note)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
+class NoteOperations:
+    def __init__(self):
         self.notes = []
+        self.load_notes()
+
+    def add_note(self, title, body):
+        note_id = len(self.notes) + 1
+        note = Note(note_id, title, body)
+        self.notes.append(note)
+        self.save_notes()
+        print('Заметка успешно сохранена')
+
+
+    def read_notes_by_date(self):
+        date = input("Введите дату (ГГГГ-ММ-ДД): ")
+        # try:
+        #     date_check = datetime.strptime(date, '%d-%m-%Y').date()
+        # except ValueError:
+        #     raise ValueError('Invalid date format. Please enter the date in dd-mm-yy format.')
+
+        filter_notes = []
+        for note in self.notes:
+            if note.date.split()[0] == date:
+                filter_notes.append(note)
+        if not filter_notes:
+            print("Заметок нет")
+        return filter_notes
+
+    def view_filter_note(self, note_id, note):
+        print(f"ID: {note.note_id}")
+        print(f"Title: {note.title}")
+        print(f"Body: {note.body}")
+        print(f"Date: {note.date}")
+
+    def view_all_notes(self):
+        if self.notes:
+            print("Все заметки")
+            for note in self.notes:
+                print(note)
+            input("Нажмите номер для продолжения")
+        else:
+            print("Заметки не найдены")
+    def edit_note_tit(self, note_id, new_title):
+        for note in self.notes:
+            if note.note_id == note_id:
+                note.title = new_title
+                note.date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                self.save_notes()
+                print("Заголовок заметки успешно отредактирована")
+                return
+        print(f"Заметка с id {note_id} не найдена")
+
+    def edit_note_body(self, note_id, new_body):
+        for note in self.notes:
+            if note.note_id == note_id:
+                note.body = new_body
+                note.date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                self.save_notes()
+                print("Заметка успешно отредактирована")
+                return
+        print(f"Заметка с id {note_id} не найдена")
+
+    def delete_note(self, note_id):
+        for note in self.notes:
+            if note.note_id == note_id:
+                self.notes.remove(note)
+                self.save_notes()
+                print("Заметка удалена")
+                return
+        print(f"Заметка с id {note_id} не найдена")
+
+    def save_notes(self):
+        data = []
+        for note in self.notes:
+            note_data = {'note_id': note.note_id, 'title': note.title, 'body': note.body, 'date': note.date}
+            data.append(note_data)
+        with open("notes.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+    def load_notes(self):
+        try:
+            with open("notes.json", "r") as file:
+                data = json.load(file)
+                for note_data in data:
+                    note = Note(note_data['note_id'], note_data['title'], note_data['body'], note_data['data'])
+                    self.notes.append(note)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            self.notes = []
