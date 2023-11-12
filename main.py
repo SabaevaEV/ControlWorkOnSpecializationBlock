ec936763227c33f2a208ecc169417f94acd61443
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 11:19:45 +0400

    Added changes

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from Operations import *
+from View import View
 
+if __name__ == "__main__":
+    notes = load_notes()
+    while True:
+        View()
+        command = View.menu
+        if command == "1":
+            add_note()
+        elif command == "2":
+            read_notes()
+        elif command == "3":
+            edit_note()
+        elif command == "4":
+            delete_note()
+        elif command == "5":
+            break
+        else:
+            print("Неверная команда")
