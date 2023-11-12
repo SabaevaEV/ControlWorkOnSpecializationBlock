d70978cae25f3a8cfdf5f063e2d90b514299a6a8
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 11:53:53 +0400

    Added changes

--- note.py
+++ note.py
@@ -2,15 +2,8 @@ from datetime import datetime
 
 class Note:
 
-    def __init__(self, body, title):  # Инициализация класса с полями (Конструктор Заметки)
-        self.date = datetime.now()  # Присваивание текущей даты и времени заметке
-        self.note_id = id(self.date)  # Присваивание ID (по дате) заметке
-        self.body = body  # Ввод тела заметки
-        self.title = title  # Ввод заголовка заметки
-
-    def __init__(self, note_id, title, body, created_time, modified_time):
-        self.note_id = note_id
-        self.title = title
+    def __init__(self, body, title):
+        self.date = datetime.now()
+        self.note_id = id(self.date)
         self.body = body
-        self.created_time = created_time
-        self.modified_time = modified_time
+        self.title = title
