9c23e9947709b1ee652e81e550c8fd683b41f82b
SabaevaEvgeniya <eva230391@mail.ru>
2023-11-12 11:53:09 +0400

    Added changes

--- note.py
+++ note.py
@@ -7,3 +7,10 @@ class Note:
         self.note_id = id(self.date)  # Присваивание ID (по дате) заметке
         self.body = body  # Ввод тела заметки
         self.title = title  # Ввод заголовка заметки
+
+    def __init__(self, note_id, title, body, created_time, modified_time):
+        self.note_id = note_id
+        self.title = title
+        self.body = body
+        self.created_time = created_time
+        self.modified_time = modified_time
--- operations.py
+++ operations.py
@@ -1,25 +1,18 @@
 import json
 from datetime import datetime
+from note import Note
 
 
+def __init__(self):
+    self.notes = []
+    self.load_notes()
 
-def add_note():
-    title = input("Введите заголовок заметки: ")
-    body = input("Введите тело заметки: ")
-    note_id = len(notes) + 1
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    notes.append({"id": note_id, "title": title, "body": body, "date": date})
-    save_notes()
-    print("Заметка успешно сохранена")
-
-    # def add_note(self, title, body):
-    #     note_id = len(self.notes) + 1
-    #     created_time = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-    #     modified_time = created_time
-    #     note = Note(note_id, title, body, created_time, modified_time)
-    #     self.notes.append(note)
-    #     self.save_notes()
-    #     print('Note added successfully.')
+ def add_note(self, title, body):
+    note_id = len(self.notes) + 1
+    note = Note(note_id, title, body)
+    self.notes.append(note)
+    self.save_notes()
+    print('Заметка успешно сохранена')
 
 def read_notes():
     filter_date = input("Введите дату для фильтрации (в формате ГГГГ-ММ-ДД): ")
